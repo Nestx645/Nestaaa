7c39ec82ad3ea06f76678e50384a7a0a6e2d1b91
Xtsen <xts.enn@gmail.com>
2021-05-31 22:54:35 +0200

    add images

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -8,11 +8,12 @@ function animLoad(){
 
 // Slider
 
-const websites = ['Everyweb', 'Le site SFDA', 'Ekip', 'Help me', 'Workout']
+const websites = ['Everyweb', 'SFDA love', 'Ekip', 'Help me', 'Workout']
 const Languages = ['HTML/CSS/JS', 'HTML/CSS/JS/PHP', 'HTML/CSS/JS', 'HTML/CSS/JS', 'HTML/CSS/JS']
 const projectDuration = ['100h', '60h', '20h', '10h', '4-5h']
-const backgrounds = ['#ffc15e', '#DE3636', '#DE36A1', '#3636DE', '#36DEC5']
+const backgrounds = ['#ffc15e', '#36D9DE', '#DE36A1', '#9936DE', '#3694DE']
 const colors = ['#222', '#fff', '#222', '#fff', '#222']
+const images = ['res/backgroundEveryweb.png', 'res/backgroundSFDALove.png', 'res/backgroundEkip.png', 'res/backgroundHelpME.png', 'res/backgroundWorkout.png']
 
 function verifCurrent() {
     current = document.getElementById('siteName').innerText
@@ -41,9 +42,12 @@ function animationNext() {
     document.getElementById("languagesRequire").classList.add("animNext");
     document.getElementById("siteName").classList.add("animNext");
     document.getElementById("timeToDo").classList.add("animNext");
+    document.getElementById("imgSite").classList.add("animNext");
     document.getElementById('slider').style.backgroundColor = backgrounds[currentIndexWebsite];
     document.getElementById('slider').style.color = colors[currentIndexWebsite];
+    setTimeout(function(){ document.getElementById("imgSite").setAttribute('src', images[currentIndexWebsite]) }, 500);
     setTimeout(function(){ document.getElementById("languagesRequire").classList.remove("animNext"); }, 1000);
     setTimeout(function(){ document.getElementById("siteName").classList.remove("animNext"); }, 1000);
     setTimeout(function(){ document.getElementById("timeToDo").classList.remove("animNext"); }, 1000);
+    setTimeout(function(){ document.getElementById("imgSite").classList.remove("animNext"); }, 1000);
 }
