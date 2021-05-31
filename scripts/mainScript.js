a7e86f4295b1dfc634becdaf7a8015c90d11d3e6
Xtsen <xts.enn@gmail.com>
2021-05-31 18:16:18 +0200

    initial commit

---
+++ scripts/mainScript.js
@@ -0,0 +1,6 @@
+function animLoad(){
+    document.getElementById("logo").classList.add("animX");
+    setTimeout(function(){ document.getElementById("heroTitle").classList.add("animX"); }, 300);
+    setTimeout(function(){ document.getElementById("heroSubTitle").classList.add("animX"); }, 300);
+    setTimeout(function(){ document.getElementById("SVG").classList.add("animX"); }, 600);
+}
